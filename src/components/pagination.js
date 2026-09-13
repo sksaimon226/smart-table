92295b54c9956e037fc3a77e58583d84301613ab
Artyom Agafonov <saimon110099@yandex.ru>
2026-09-13 13:05:30 +0300

    7 Pr

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -33,12 +33,12 @@ export const initPagination = ({ pages, fromRow, toRow, totalRows }, createPage)
 
         // @todo: #2.5 — обновить статус пагинации
         fromRow.textContent = (page - 1) * limit + 1;                    // С какой строки выводим
-        toRow.textContent = Math.min((page * limit), data.length);    // До какой строки выводим, если это последняя страница, то отображаем оставшееся количество
-        totalRows.textContent = data.length;
+        toRow.textContent = Math.min((page * limit), total);    // До какой строки выводим, если это последняя страница, то отображаем оставшееся количество
+        totalRows.textContent = total;
         // Сколько всего строк выводим на всех страницах вместе (после фильтрации будет меньше)
         // @todo: #2.2 — посчитать сколько строк нужно пропустить и получить срез данных
-        const skip = (page - 1) * rowsPerPage;            // сколько строк нужно пропустить
-        return data.slice(skip, skip + rowsPerPage);    // получаем нужную часть строк (заменяем имеющийся return)
+        const skip = (page - 1) * limit;            // сколько строк нужно пропустить
+        
     }
     return {
         updatePagination,
